61faba69a7fe6267abfd43ee119d9a1e9b8ea0fe
fernvilla <fvilla18@gmail.com>
2014-11-29 19:04:13 -0800

    Add favicon and dynamic page titles

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+  def full_title(page_title)
+    base_title = "Ariana and Fernando's Wedding - June 26, 2015"
+
+    if page_title.empty?
+        base_title
+    else
+        "#{base_title} | #{page_title}"
+    end
+  end
 end
