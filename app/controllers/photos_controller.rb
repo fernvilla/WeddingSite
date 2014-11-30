c1bbaf3118e82bae8878d31994789999ec77862a
fernvilla <fvilla18@gmail.com>
2014-11-29 22:34:16 -0800

    Split photos into own galleries

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -1,5 +1,8 @@
 class PhotosController < ApplicationController
   def index
+  end
+
+  def engagement
     @images = Dir.glob("app/assets/images/photos/engagement/*.jpg")
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   root 'home#index'
 
   get 'photos' => 'photos#index'
+  get 'photos/engagement' => 'photos#engagement'
   get 'reception' => 'reception#index'
   get 'ceremony' => 'ceremony#index'
 
