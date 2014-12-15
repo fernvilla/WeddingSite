f77f7707ab21d52fcc88e8d249222aad333c5984
fernvilla <fvilla18@gmail.com>
2014-12-14 21:37:15 -0800

    Add isotope for messages

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -13,6 +13,7 @@
 //= require jquery
 //= require jquery_ujs
 //= require lightbox/lightbox.min.js
+//= require isotope.min.js
 //= require jquery.lazyload.js
 //= require bootstrap-sprockets
 //= require turbolinks
--- app/assets/javascripts/messages.js
+++ app/assets/javascripts/messages.js
@@ -1,2 +1,10 @@
 // Place all the behaviors and hooks related to the matching controller here.
 // All this logic will automatically be available in application.js.
+
+// Isotope
+var $container = $('#container');
+
+$container.isotope({
+  itemSelector: '.message-box',
+  layoutMode: 'masonry'
+});
